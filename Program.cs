9221e5b7f603a7f89de28c39db3dcd9eb9e24e92
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-10-07 16:51:45 +0530

    Main function

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileFragmentation
+{
+    internal class Program
+    {
+        static void Main(string[] args) { 
+        
+
+            FileController controller = new FileController();
+            controller.StartProcess();
+        }
+    }
+}
